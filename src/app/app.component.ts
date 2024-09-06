106709f7bdd802800935f92d0a51dfe0b4bd5542
Alireza <thisisaalireza@gmail.com>
2024-09-06 10:27:48 +0330

    add english font add social media links add gif banner in homepage and etc...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+// Initialization for ES Users
+import { Collapse, initMDB } from 'mdb-ui-kit';
 
+initMDB({ Collapse });
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
